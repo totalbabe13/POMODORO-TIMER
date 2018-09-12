5e3b2897e6809ce30cd528458ab1d4d6f752e7eb
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-09-12 15:30:07 -0500

    creating timer functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 
 
-let subMinute = document.querySelector('#subMinute').addEventListener("click", subtractMinute);
+let subMinute = document.querySelector('#subMinute').addEventListener("click", subMintueSetter);
 let addMinute = document.querySelector('#addMinute').addEventListener("click", addingMinute);
 let minuteSetterDisplay = document.querySelector('#minutes')
 
@@ -10,22 +10,44 @@ let subMinuteBreak = document.querySelector("#subMinuteBreak").addEventListener(
 let addMinuteBreak = document.querySelector("#addMinuteBreak").addEventListener('click',addingMinuteBreak);
 let minuteSetterDisplayBreak = document.querySelector('#minutesBreak')
 
+let actualTime = document.querySelector('#actualTimer')
+
+let isTimerOn = false
+let startBUtton = document.querySelector('#pushPlay1').addEventListener("click",startTimer);
+
 let seesionTimeSetter = [25];
 let breakTimerSetter = [5];
+actualTime.textContent = seesionTimeSetter[0];
 
 minuteSetterDisplay.textContent = seesionTimeSetter;
 minuteSetterDisplayBreak.textContent = breakTimerSetter;
 
+function startTimer() {
+	
+	let intervalID = window.setInterval(function(){
+		subtractMinute();
+		if(seesionTimeSetter[0] === 0) {
+	  		clearInterval(intervalID);
+	  }
+	},1000);
+}
 
-
+function subMintueSetter () {
+	let newTime = seesionTimeSetter[0] -= 1;
+	minuteSetterDisplay.textContent = newTime;
+	actualTime.textContent = newTime;
+}
 
 
 
 function subtractMinute() {
 	let newTime = seesionTimeSetter[0] -= 1;
-    minuteSetterDisplay.textContent = newTime;
+	actualTime.textContent = newTime;
+    //minuteSetterDisplay.textContent = newTime;
 	console.log(seesionTimeSetter)
-}
+
+	} 
+
 
 function addingMinute() {
 	let newTime = seesionTimeSetter[0] += 1;
