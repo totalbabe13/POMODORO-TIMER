5569cba04ff076cd4b5a97c6ce8e3e3095a2e69a
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-09-17 21:15:44 -0500

    Fixed small bugs with timer, stop button, and added css

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 
 
 let defaultMinuteTimer = 25;
-// let defaultBreakTimer  = [5];
+let defaultBreakTimer  = 5;
 let jsTimer = 0;
+let jsBreakTimer = 0;
 let seconds = 59;
 let secondsTimer = '00';
 let pausing = false;
+let breakstop = false
 
 // - - - - - - - - - - - - - - - - - - - - - - -
 //DEFAULT TIMER SETTINGS
-let activeTimerCounter = document.querySelector('#actualTimer')
-activeTimerCounter.textContent = `${defaultMinuteTimer}:${secondsTimer}`
+let activeTimerCounter = document.querySelector('#actualTimer');
+activeTimerCounter.textContent = `${defaultMinuteTimer}:${secondsTimer}`;
 
 // - - - - - - - - - - - - - - - - - - - - - - -
-//DEFAULT SETTING FOR SESSION -->25
-let sessionMinutesSetter = document.querySelector('#minuteSetter')
-sessionMinutesSetter.textContent = defaultMinuteTimer ;
+//DEFAULT SETTING FOR SESSION  and BREAK -->25 / 5
+let sessionMinutesSetter = document.querySelector('#minuteSetter');
+sessionMinutesSetter.textContent = defaultMinuteTimer;
+let breakMintuesSetter = document.querySelector('#minutesBreak');
+breakMintuesSetter.textContent = defaultBreakTimer
 // - - - - - - - - - - - - - - - - - - - - - - -
 // SESSION BUTTONS -->ADD/ SUBTRACT
 
@@ -35,6 +39,24 @@ function addOneMinuteToSession() {
 	console.log(defaultMinuteTimer)
 }
 // - - - - - - - - - - - - - - - - - - - - - - -
+// BREAK BUTTONS -->ADD/ SUBTRACT
+
+//SUBTRACT BUTTON 
+let subMinuteBreakButton = document.querySelector('#subMinuteBreak').addEventListener("click", subOneMinuteToBreak);
+function subOneMinuteToBreak() {
+	defaultBreakTimer -= 1;
+	breakMintuesSetter.textContent = defaultBreakTimer;
+	console.log(defaultMinuteTimer)
+};
+
+let addMinuteBreakButton = document.querySelector('#addMinuteBreak').addEventListener("click", addOneMinuteToBreak);
+function addOneMinuteToBreak() {
+	defaultBreakTimer += 1;
+	breakMintuesSetter.textContent = defaultBreakTimer;
+	console.log(defaultMinuteTimer)
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - -
 //START BUTTON --> run timer!
 let intervalID;
 let startButton = document.querySelector('#pushPlay')
@@ -42,74 +64,37 @@ startButton.addEventListener("click",startTimer)
 
 function startTimer() {
 	convertMinutesToJStime(defaultMinuteTimer);
-	timerDisplay();
+	timerDisplay(jsTimer);
 	pausing = false
 	
-	let intervalID = setInterval(myCallback, 1000);
-    function myCallback() {
-
-    	if (jsTimer < 1 || pausing === true){
-    		clearInterval(intervalID);
-          }
-
-        jsTimer -= 1;
+	let intervalID = setInterval(runTimer, 100);
+    function runTimer() {
+    	jsTimer -= 1;
         seconds -= 1;
-        timerDisplay(jsTimer);  
+        timerDisplay(jsTimer);
+
+    	if (jsTimer < 1 ){
+    		convertMinutesToJStime(defaultBreakTimer);
+    		timerDisplay(jsTimer);
+    		breakstop = true
+    		console.log(jsTimer)
+    		    if (jsTimer === 1 ){
+    		       activeTimerCounter.textContent = `00:00`;
+    		       clearInterval(intervalID);    		       
+    		    };
+          } else if(pausing === true) {
+          	  clearInterval(intervalID);
+          }
     }
 };
 
 // - - - - - - - - - - - - - - - - - - - - - - -
-//PAUSE BUTTON --> pause timer!
-let pauseButton = document.querySelector('#pushPause')
-pauseButton.addEventListener("click",pauseTimer)
+//HELPER FUNCTION --> !
 
-function pauseTimer() {
-	pausing = true
-	console.log(pausing)
-};
-
-
-// - - - - - - - - - - - - - - - - - - - - - - -
-//STOP BUTTON --> stop timer!
-let stopButton = document.querySelector('#pushStop')
-stopButton.addEventListener("click",stopTimer)
-
-function stopTimer() {
-
-	defaultMinuteTimer = 25;
-    jsTimer = 0;
-    seconds = 00;
-    secondsTimer = '00';
-    pausing = true;
-    
-    convertMinutesToJStime(defaultMinuteTimer)
-    timerDisplay(jsTimer);
-
-
-	console.log('STOPING')
-};
-
-
-// - - - - - - - - - - - - - - - - - - - - - - -
-
-
-
-// let jsTimer = 0;
-// let inputTimefromUser = 2;
-// let minDisplay = 0;
-// let seconds = 60;
-// let secondsTimer;
-
-// //convert mintues from user into a JS format
 // // NOTE** --> counts down in seconds (1000 = 1 second)
 function convertMinutesToJStime(UserMinutes) {
   jsTimer = Number(UserMinutes) * 60
 };
-
-
-// // COUNTDOWN mechanism
-// // 1. Convert USER input into JS timer
-// convertMinutesToJStime(inputTimefromUser);
 
 // //CONVERT SECONDS to display: 9sec --> 09 (11:09)
 function convertSeconds(currentSeconds){
@@ -127,18 +112,34 @@ function timerDisplay(jsTimer) {
    let displayMinutes = (Math.floor(jsTimer/60));
    convertSeconds(seconds);
    activeTimerCounter.textContent = `${displayMinutes}:${secondsTimer}`;
-   console.log(`${displayMinutes}:${secondsTimer}`)
+   // console.log(`${displayMinutes}:${secondsTimer}`)
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - -
+//PAUSE BUTTON --> pause timer!
+let pauseButton = document.querySelector('#pushPause')
+pauseButton.addEventListener("click",pauseTimer)
+
+function pauseTimer() {
+	pausing = true
+	// console.log(pausing)
 };
 
 
-// // 3.Let the countdown change display
-// var intervalID = window.setInterval(myCallback, 100);
-// function myCallback() {
-//   if (jsTimer < 1){
-//     clearInterval(nIntervId);
-//   } 
-//   jsTimer -= 1;
-//   seconds -= 1;
-//   timerDisplay(jsTimer)
-  
-// };
+// - - - - - - - - - - - - - - - - - - - - - - -
+//STOP BUTTON --> stop timer!
+let stopButton = document.querySelector('#pushStop')
+stopButton.addEventListener("click",stopTimer)
+
+function stopTimer() {
+	defaultMinuteTimer = 25;
+    jsTimer = 0;
+    seconds = 0;
+    secondsTimer = '00';
+    pausing = true;
+    
+    convertMinutesToJStime(defaultMinuteTimer)
+    timerDisplay(jsTimer);
+};
+// - - - - - - - - - - - - - - - - - - - - - - -
+
